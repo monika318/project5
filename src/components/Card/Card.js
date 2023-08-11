f2aea5f329e1ad2ba2f6bb42275b79ff81f90205
Monika Shakya <59469056+monika318@users.noreply.github.com>
2023-08-11 14:43:17 +0545

    Commit Before I push the cart inside card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import styles from './Card.module.css'
 import * as  AiIcons from 'react-icons/ai'
+import { useNavigate } from 'react-router-dom';
 
 
 const Card = (props) => {
     const { item, setShowCart } = props;
+    const navigate = useNavigate();
+
+
+
 
     const handleAddToCart = (id) => {
         // console.log(JSON.stringify(id))
@@ -27,35 +32,43 @@ const Card = (props) => {
         existingCartKeys.push(id);
         // Store the updated array of keys in local storage
         localStorage.setItem('cartKey', JSON.stringify(existingCartKeys));
-
-
+    }
+    const handleImageOnClick = (id) => {
+        // console.log(id)       
+        navigate(`/product/${id}`);
     }
     return (
         <>
-            <div className={styles.card}>
+            <div className={styles.card} >
                 <div className={styles.cardImage}>
                     <img src={item.img} alt="product1" />
-                    <div className={styles.middle}>
-                        <div className={styles.text}>
-                            <div className={styles.Icons} onClick={() => handleAddToCart(item.id)}>
-                                <AiIcons.AiOutlineShoppingCart />
-                            </div>
-                            <div className={styles.v1}></div>
-                            <div className={styles.Icons}>
-                                <AiIcons.AiFillStar />
-                            </div>
-                            <div className={styles.v1}></div>
-                            <div className={styles.Icons}>
-                                <AiIcons.AiOutlineSearch />
-                            </div>
+                    <div className={styles.overlay} onClick={() => handleImageOnClick(item.id)}>
+                    </div>
+                    <div className={styles.text}>
+                        <div className={styles.Icons} onClick={() => handleAddToCart(item.id)}>
+                            <AiIcons.AiOutlineShoppingCart />
+                        </div>
+                        <div className={styles.v1}></div>
+                        <div className={styles.Icons}>
+                            <AiIcons.AiOutlineStar />
+                        </div>
+                        <div className={styles.v1}></div>
+                        <div className={styles.Icons}>
+                            <AiIcons.AiOutlineSearch />
                         </div>
                     </div>
                 </div>
-                <div className={styles.cardBody}>
+                <div className={styles.cardBody} >
                     <h2>{item.name}</h2>
-                    <p><AiIcons.AiFillStar /><AiIcons.AiFillStar /><AiIcons.AiFillStar /><AiIcons.AiFillStar /><AiIcons.AiFillStar /></p>
+                    <p>
+                        <AiIcons.AiFillStar />
+                        <AiIcons.AiFillStar />
+                        <AiIcons.AiFillStar />
+                        <AiIcons.AiFillStar />
+                        <AiIcons.AiFillStar />
+                    </p>
                     <p>{item.price}</p>
-                    <p style={{ width: '80%', textAlign: 'center' }}>{item.description}</p>
+                    {/* {props.description && <p style={{ width: '80%', textAlign: 'center' }}>{item.description}</p>} */}
                 </div>
             </div>
         </>
